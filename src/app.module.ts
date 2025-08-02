cf86629235c2946f81ea9973a2dd293ad50fc605
Mustafa Shahid <141426131+Mustafa-Ch@users.noreply.github.com>
2025-08-02 16:08:14 +0500

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,20 +9,33 @@ import { ChatModule } from './chat/chat.module';
 import { GoogleCalendarModule } from './google-calendar/google-calendar.module';
 import { ConfigModule } from '@nestjs/config';
 import { SpeechModule } from './speech/speech.module';
+import { User } from './user/entities/user.entity';
+import { Chat } from './chat/entities/chat.entity';
+import { GoogleCalendarToken } from './google-calendar/entities/google-calendar.entity';
 
 
 @Module({
   imports: [
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   host: 'localhost',
+    //   port: 5432,
+    //   username: 'postgres',
+    //   password: 'bookd',
+    //   database: 'bookd',
+    //   entities: [__dirname + '/../**/*.entity.{ts,js}'],
+
+    //   synchronize: false,
+    // }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'bookd',
-      database: 'bookd',
-      entities: [__dirname + '/../**/*.entity.{ts,js}'],
-
-      synchronize: true,
+      host: process.env.PG_HOST, // Use the PG_HOST from env
+      port: 5432, // Default PostgreSQL port
+      username: process.env.PG_USER, // Use PG_USER from env
+      password: process.env.DB_PASSWORD, // Use DB_PASSWORD from env
+      database: 'railway', // Name of the database
+      entities: [User,Chat,GoogleCalendarToken],
+      synchronize: false, // Keep false in production
     }),
     ConfigModule.forRoot({
       isGlobal: true,
