2b4b7e4608e9d259269d90d709443f9904c449b4
Mustafa Shahid <141426131+Mustafa-Ch@users.noreply.github.com>
2025-08-02 16:20:31 +0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
+// main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
+  console.log('Bootstrap start memory (MB):', process.memoryUsage().heapUsed / 1024 / 1024);
   const app = await NestFactory.create(AppModule);
-   app.use(cookieParser());
+  console.log('After AppModule memory (MB):', process.memoryUsage().heapUsed / 1024 / 1024);
+  app.use(cookieParser());
+  console.log('After middleware memory (MB):', process.memoryUsage().heapUsed / 1024 / 1024);
   await app.listen(process.env.PORT ?? 3000);
+  console.log('After listen memory (MB):', process.memoryUsage().heapUsed / 1024 / 1024);
 }
 bootstrap();
